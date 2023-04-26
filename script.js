e0ccbcf4ba96dbc9a5c4d05fe38975e1d7799159
XY0511 <78774443+XY0511@users.noreply.github.com>
2023-04-26 11:40:33 +0800

    Added characters to calculate

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-function countWordsAndSentences() {
+function countTextStats() {
     let textInput = document.getElementById("textInput").value;
-    
+
+    // Calculate number of paragraphs
+    let numParagraphs = textInput.split(/\n+/).filter(paragraph => paragraph !== "").length;
+
     // Calculate number of words
     let words = textInput.split(/\s+/).filter(word => word !== "");
     let numWords = words.length;
-    
+
     // Calculate number of sentences
     let numSentences = textInput.split(/[.!?]+/).filter(sentence => sentence !== "").length;
-    
-    // Calculate number of paragraphs
-    let numParagraphs = textInput.split(/\n\s*\n/).filter(paragraph => paragraph !== "").length;
-    
+
+    // Calculate number of characters
+    let numCharacters = textInput.length;
+
     // Display result
     let resultDiv = document.getElementById("result");
-    resultDiv.innerHTML = "Number of words: " + numWords + "<br>" + "Number of sentences: " + numSentences + "<br>" + "Number of paragraphs: " + numParagraphs;
+    resultDiv.innerHTML = "Number of paragraphs: " + numParagraphs + "<br>" + "Number of words: " + numWords + "<br>" + "Number of sentences: " + numSentences + "<br>" + "Number of characters: " + numCharacters;
 }
